6fafb8826ff1593a589eb673a8942b67595f5f48
Ian <ian@TOMATO.PC>
2025-09-10 16:56:26 -0300

    fixed bugs

--- labo_01.py
+++ labo_01.py
@@ -31,7 +31,7 @@ def error(x,y):
     #Recibe dos numeros x e y, y calcula el error de aproximar x usando y en float64
 
 def error_relativo(x,y):
-    return np.abs(x-y/x)
+    return np.abs(error(x,y)/np.abs(x))
     #Recibe dos numeros x e y, y calcula el error relativo de aproximar x usando y en float64
     
 def matricesIguales(A:Matriz,B:Matriz):
@@ -40,12 +40,32 @@ def matricesIguales(A:Matriz,B:Matriz):
     if A.shape != B.shape: return False
     for x in range(A.shape[0]):
         for y in range(A.shape[1]):
-            if A[x][y] != B[x][y]:
+            a= A[x][y]
+            b= B[x][y]
+            if error(A[x][y],B[x][y])>= 1e-8:
                 return False
     return True  
-    
-    
-    
-    
-    
-main()
+
+import numpy as np
+
+def sonIguales(x, y, atol=1e-8):
+    return np.allclose(error(x, y), 0, atol=atol)
+
+# Tests para sonIguales
+assert not sonIguales(1, 1.1)
+assert sonIguales(1, 1 + np.finfo('float64').eps)
+assert not sonIguales(1, 1 + np.finfo('float32').eps)
+assert not sonIguales(np.float16(1), np.float16(1) + np.finfo('float32').eps)
+assert sonIguales(np.float16(1), np.float16(1) + np.finfo('float16').eps, atol=1e-3)
+
+# Tests para error relativo
+assert np.allclose(error_relativo(1, 1.1), 0.1)
+assert np.allclose(error_relativo(2, 1), 0.5)
+assert np.allclose(error_relativo(-1, -1), 0)
+assert np.allclose(error_relativo(1, -1), 2)
+
+# Tests para matricesIguales
+assert matricesIguales(np.diag([1, 1]), np.eye(2))
+assert(matricesIguales(np.linalg.inv(np.array([[1,2],[3,4]]))@np.array([[1,2],[3,4]]),np.eye(2)))
+assert not matricesIguales(np.array([[1, 2], [3, 4]]).T, np.array([[1, 2], [3, 4]]))
+
